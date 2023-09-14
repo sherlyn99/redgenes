110925892df469b93043912faec731ff276fc5b0
roles <roles@health.ucsd.edu>
2023-09-14 09:32:14 -0700

    created package structure

--- scripts/bakta_main.py
+++ scripts/bakta_main.py
@@ -1,32 +1,41 @@
-# database_operations.py
-# Implement functions for database operations (create, insert, check)
-
-# submit_jobs.py
-# Submit Bakta jobs and collect job information
-
-# process_bakta_output.py
-# Process Bakta output, insert data into the Bakta output database
-
 # main.py
-from database_operations import create_databases, insert_software, insert_run
-from submit_jobs import submit_bakta_jobs, monitor_job_status
-from process_bakta_output import process_bakta_output
 
-# Step 1: Create databases and tables
-create_databases()
+import argparse
+from bakta_package import database_operations, submit_jobs, process_bakta_output
 
-# Step 2: Submit Bakta jobs and collect job information
-job_info = submit_bakta_jobs()
+def main(fasta_file, bakta_output_dir, software_info, run_info):
+    # Step 1: Create or initialize the databases
+    database_operations.create_databases()
 
-# Step 3: Monitor job status and wait for jobs to finish
-monitor_job_status(job_info)
+    # Step 2: Submit a Bakta job for the provided FASTA file
+    job_info = submit_jobs.submit_bakta_job(fasta_file)
+    updated_job_info = submit_jobs.monitor_job_status(job_info)
 
-# Step 4: Process Bakta output and update the database
-for job in job_info:
-    bakta_output = retrieve_bakta_output(job)
-    process_bakta_output(bakta_output)
+    # Step 3: Process Bakta output and update the Bakta output database
+    for job in updated_job_info:
+        bakta_output_file = f"{bakta_output_dir}/{job['sample']}.gff"
+        process_bakta_output.process_bakta_output(bakta_output_file)
+        process_bakta_output.delete_bakta_output_files(bakta_output_file)
 
-    # Optionally, delete Bakta output files
-    delete_bakta_output_files(bakta_output)
+    # Step 4: Insert software and run information into the databases
+    database_operations.insert_software(software_info)
+    database_operations.insert_run(run_info)
 
-# End of the workflow
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Bakta Workflow")
+    parser.add_argument("fasta_file", help="Path to the FASTA file")
+    parser.add_argument("bakta_output_dir", help="Path to the Bakta output directory")
+    parser.add_argument("software_name", help="Name of the software")
+    parser.add_argument("software_version", help="Version of the software")
+    parser.add_argument("software_arguments", help="Arguments used with the software")
+    parser.add_argument("software_description", help="Description of the software")
+    parser.add_argument("run_accession", help="Run accession")
+    parser.add_argument("slurm_job_id", help="SLURM job ID")
+    parser.add_argument("run_timestamp", help="Run timestamp")
+    
+    args = parser.parse_args()
+
+    software_info = (args.software_name, args.software_version, args.software_arguments, args.software_description)
+    run_info = (args.run_accession, args.slurm_job_id, args.run_timestamp)
+
+    main(args.fasta_file, args.bakta_output_dir, software_info, run_info)
