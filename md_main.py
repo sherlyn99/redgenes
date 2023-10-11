d16412ac1032b3472432b94e46db7a7fc581311d
sherlyn99 <y1weng@ucsd.edu>
2023-10-11 00:17:06 -0700

    updated md_main.py and md_unittest.py based on previous pr

--- md_main.py
+++ md_main.py
@@ -1,17 +1,19 @@
-import argparse
+import click
 import logging
 import pandas as pd
-import pathlib
-import shutil
 import sqlite3
 from os import path
 
-from scripts.database_operations import *
+from scripts.database_operations import create_databases
 
-# todo: turn all print and error statements into logging
-# test run: python md_main.py metadata -m ./test/data/gordon_db_ready.tsv --db "test.db"
+logging.basicConfig(
+    filename='md_main.log',
+    level=logging.DEBUG, 
+    format='%(asctime)s:%(name)s:%(levelname)s:%(message)s')
 
-EXPECTED_COLS_MD = [
+# test run: python md_main.py metadata --md ./test/data/gordon_db_ready.tsv --db "test.db"
+
+EXPECTED_COLS_MD = frozenset([
     "external_source",
     "external_accession",
     "filename",
@@ -37,90 +39,86 @@ EXPECTED_COLS_MD = [
     "ftp_path",
     "excluded_from_refseq",
     "relation_to_type_material",
-    "asm_not_live_date"
-    ]
+    "asm_not_live_date"])
 
-INSERT_COLS_IDENTIFIER = [
+INSERT_COLS_IDENTIFIER = frozenset([
     'filename',
     'filepath',
     'external_accession',
-    'external_source'
-]
+    'external_source'])
 
-INSERT_COLS_MD = EXPECTED_COLS_MD.copy()
-INSERT_COLS_MD.remove('filename')
-INSERT_COLS_MD.remove('filepath')
+INSERT_COLS_MD = EXPECTED_COLS_MD - {'filename', 'filepath'}
 
 def check_input(path_md):
     """Checks if the input md file is valid and returns metadata as a pandas dataframe."""
     if path.isfile(path_md):
         md = pd.read_csv(path_md, sep='\t')
-        actual_cols = md.columns.to_list()
-        print(actual_cols)
-        actual_cols_n, expected_cols_n = len(actual_cols), len(EXPECTED_COLS_MD)
-        if actual_cols_n == expected_cols_n:
-            for i in range(actual_cols_n):
-                if actual_cols[i] != EXPECTED_COLS_MD[i]:
-                    raise IOError(f'Metadata does not contain expected headers. Check {actual_headers[i]}')
-        else:
-            raise IOError(f'Metadata does not have {expected_cols_n} columns.')
-        print('Metadata loaded')
+        actual_cols = set(md.columns.to_list())
+        if actual_cols != EXPECTED_COLS_MD:
+            raise IOError(f'Metadata does not contain expected headers, missing {EXPECTED_COLS_MD - actual_cols}')
+        logging.info('metadata valid and loaded')
         return md
     else: 
         raise FileNotFoundError(path_md)
 
-def insert_into_identifier(conn, md):
-    """Insert relevant columns into identifiers and return corresponding entity ids."""
-    cursor = conn.cursor()
-    _fileinfo = md[INSERT_COLS_IDENTIFIER]
 
-    # get existing entity_id
-    cursor.execute("SELECT MAX(entity_id) FROM identifier")
-    max_existing_id = cursor.fetchone()[0]
-    if max_existing_id is None:
-        max_existing_id = 0
-
-    # insert new records into identifier
-    _fileinfo.to_sql('identifier', conn, if_exists='append', index=False)
-
-    # extract entity ids of newly added records
-    cursor.execute(f"SELECT entity_id FROM identifier WHERE entity_id > {max_existing_id}")
-    entity_ids = [row[0] for row in cursor.fetchall()]
-
-    print('Table identifier updated')
-    return entity_ids
-
-def insert_into_metadata(conn, md, entity_ids):
-    """Insert metadata into the metadata table."""
-    _md = md[INSERT_COLS_MD]
-    _entity_ids_df = pd.DataFrame({'entity_id': entity_ids})
-    _md = pd.concat([_entity_ids_df, _md], axis=1)
-    _md.to_sql('metadata', conn, if_exists='append', index=False)
-    print('Table metadata loaded')
-    return
-
-
+@click.group()
 def cli():
-    parser = argparse.ArgumentParser(prog='md_main')
-    subparsers = parser.add_subparsers(title = 'commands')
+    pass
 
-    parser_group1 = subparsers.add_parser('metadata', help='Insert metadata in batches')
-    parser_group1.add_argument('-m', '--md', metavar='', type=pathlib.Path, help='Path to the metadata file (.tsv)')
-    # --db will be deleted once we finalize the location of the db file
-    parser_group1.add_argument('-db', '--db', metavar='', type=pathlib.Path, help='Path to the database')
+@cli.command()
+@click.option(
+    '--md',
+    type=click.Path(exists=True, dir_okay=False),
+    help='Path to the metadata file (.tsv)')
+@click.option(
+    '--db',
+    type=click.Path(dir_okay=False),
+    help='Path to the database')
+def metadata(md, db):
+    """Insert metadata into the metadata table."""
+    # --db and the following line to be removed once we finalize the db path
+    create_databases(db)
+    conn = sqlite3.connect(db)
+    cursor = conn.cursor()
+    # check metadata
+    metadata = check_input(md)
 
-    parser_group2 = subparsers.add_parser('quast', help='Run quast results in batches')
-    parser_group2.add_argument('-i', '--input', metavar='', type=pathlib.Path, help='Path to the genome directory')
-
-    args = parser.parse_args()
+    try:
+        # insert into identifier and grab entity ids
+        _fileinfo = metadata[INSERT_COLS_IDENTIFIER]
+        entity_ids = []
+        for _, row in _fileinfo.iterrows():
+            cursor.execute(
+                '''
+                insert into identifier(filename, filepath, external_accession, external_source)
+                values(?, ?, ?, ?)
+                returning entity_id
+                ''',
+                (row['filename'], row['filepath'], row['external_accession'], row['external_source'])
+            )
+            entity_id = cursor.fetchone()[0]
+            entity_ids.append(entity_id)
+        logging.info('table identifier updated')
     
-    # Metadata functions
-    create_databases(args.db)
-    conn = sqlite3.connect(args.db)
-    md = check_input(args.md)
-    eids = insert_into_identifier(conn, md)
-    insert_into_metadata(conn, md, eids)
+        # insert into metadata
+        _md = metadata[INSERT_COLS_MD]
+        _entity_ids_df = pd.DataFrame({'entity_id': entity_ids})
+        _md = pd.concat([_entity_ids_df, _md], axis=1)
+        _md.to_sql('metadata', conn, if_exists='append', index=False)
+        logging.info('table metadata updated')
+
+        conn.commit()
+    except Exception as e:
+        conn.rollback()
+        raise e
+    
     conn.close()
+
+@cli.command()
+@click.option('--input')
+def quast(input):
+    return
 
 if __name__ == '__main__':
     cli()
--- md_unittest.py
+++ md_unittest.py
@@ -1,134 +1,44 @@
 import os
 import pandas as pd
 import sqlite3
-from tempfile import TemporaryDirectory
 import unittest
+from click.testing import CliRunner
 
-from md_main import check_input, insert_into_identifier, insert_into_metadata
-
+from scripts.database_operations import create_databases
+from md_main import check_input, metadata
 
 class TestMdMain(unittest.TestCase):
 
-    def setUp(self):
-        # Create a temporary directory for test output files
-        self.temp_dir = TemporaryDirectory()
-        self.db_connection = sqlite3.connect(':memory:')  # In-memory database
-        self.db_cursor = self.db_connection.cursor()
-
-        # Create the necessary tables for testing
-        self.db_cursor.execute('''
-            CREATE TABLE IF NOT EXISTS identifier(
-                entity_id integer primary key autoincrement,
-                filename varchar not null,
-                filepath varchar not null,
-                external_accession varchar,
-                external_source varchar,
-                active int default 1,
-                created_at timestamp default current_timestamp,
-                unique(filename, filepath),
-                unique(external_accession, external_source)
-            )
-        ''')
-
-        self.db_cursor.execute('''
-            CREATE TABLE IF NOT EXISTS metadata (
-                md_id integer primary key autoincrement,
-                entity_id integer,
-                external_source varchar,
-                external_accession varchar,
-                bioproject varchar,
-                biosample varchar,
-                wgs_master varchar,
-                refseq_category varchar,
-                taxid integer,
-                species_taxid varchar,
-                organism_name varchar,
-                infraspecific_name varchar,
-                isolate varchar,
-                version_status varchar,
-                assembly_level varchar,
-                release_type varchar,
-                genome_rep varchar,
-                seq_rel_date varchar,
-                asm_name varchar,
-                submitter varchar,
-                gbrs_paired_asm varchar,
-                paired_asm_comp varchar,
-                ftp_path varchar,
-                excluded_from_refseq varchar,
-                relation_to_type_material varchar,
-                asm_not_live_date varchar,
-                created_at timestamp default current_timestamp,
-                foreign key (entity_id) references identifier (entity_id)
-            )
-        ''')
-
-    def tearDown(self):
-        # Clean up temporary files and directories
-        self.temp_dir.cleanup()
-        # Close the in-memory database connection
-        self.db_connection.close()
-
     def test_check_input(self):
-        # Test the check_input function with a valid metadata file
+        '''Test the check_input function with a valid metadata file'''
         metadata_file = './test/data/gordon_db_ready.tsv'
         md = check_input(metadata_file)
         self.assertIsInstance(md, pd.DataFrame)
         self.assertEqual(md.shape, (91,26))
 
-    def test_insert_into_identifier(self):
-        # Test the insert_into_identifier function
-        md = pd.DataFrame({
-            'filename': ['file1', 'file2'],
-            'filepath': ['path1', 'path2'],
-            'external_accession': ['acc1', 'acc2'],
-            'external_source': ['source1', 'source2']
-        })
-        entity_ids = insert_into_identifier(self.db_connection, md)
-        self.assertEqual(len(entity_ids), 2)
+    def test_metadata_command(self):
+        runner = CliRunner()
+        metadata_file = './test/data/gordon_db_ready.tsv'
+        db_path = 'test.db'
+        create_databases(db_path)
+        result = runner.invoke(metadata, ['--md', metadata_file, '--db', db_path])
+        print(result.output)
+        print(result.exc_info)
+        self.assertEqual(result.exit_code, 0)
 
-    def test_insert_into_metadata(self):
-        # Test the insert_into_metadata function
-        input_md = pd.DataFrame({
-            "external_source": ["source1"],
-            "external_accession": ["aaaa"],
-            "filename": ["aaaa"],
-            "filepath": ["aaaa"],
-            "bioproject": ["aaaa"],
-            "biosample": ["aaaa"],
-            "wgs_master": ["aaaa"],
-            "refseq_category": ["aaaa"],
-            "taxid": ["aaaa"],
-            "species_taxid": ["aaaa"],
-            "organism_name": ["aaaa"],
-            "infraspecific_name": ["aaaa"],
-            "isolate": ["aaaa"],
-            "version_status": ["aaaa"],
-            "assembly_level": ["aaaa"],
-            "release_type": ["aaaa"],
-            "genome_rep": ["aaaa"], 
-            "seq_rel_date": ["aaaa"],
-            "asm_name": ["aaaa"],
-            "submitter": ["aaaa"],
-            "gbrs_paired_asm": ["aaaa"],
-            "paired_asm_comp": ["aaaa"],
-            "ftp_path": ["aaaa"],
-            "excluded_from_refseq": ["aaaa"],
-            "relation_to_type_material": ["aaaa"],
-            "asm_not_live_date": ["aaaa"]
-        })
-        insert_into_metadata(self.db_connection, input_md, [2])
-        sql_query = 'SELECT * FROM metadata'
-        output_md = pd.read_sql_query(sql_query, self.db_connection)
-        _entity_ids_df = pd.DataFrame({'entity_id': [2]})
-        _md_ids_df = pd.DataFrame({'md_id': [1]})
-        input_md = pd.concat([_md_ids_df, _entity_ids_df, input_md], axis=1)
-        input_md = input_md.drop(['filename', 'filepath'], axis=1)
-        output_md = output_md.drop('created_at', axis=1)
-        print(input_md)
-        print(output_md)
-        self.assertTrue(output_md.equals(input_md))
+        test_db_connection = sqlite3.connect(db_path)
+        cursor = test_db_connection.cursor()
+        cursor.execute('select * from identifier')
+        rows = cursor.fetchall()
+        self.assertEqual(len(rows), 91)
 
+        cursor.execute('select * from metadata')
+        rows = cursor.fetchall()
+        self.assertEqual(len(rows), 91)
+
+        test_db_connection.close()
+        os.remove('test.db')
+        os.remove('md_main.log')
+    
 if __name__ == '__main__':
     unittest.main()
-
