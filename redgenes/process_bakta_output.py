79ba66a0ca6c20ba3b5a0e7c11a56089df703504
roles <roles@health.ucsd.edu>
2024-01-12 09:03:33 -0800

    updated script

--- redgenes/process_bakta_output.py
+++ redgenes/process_bakta_output.py
@@ -1,120 +1,136 @@
 import os
-import shutil
-from BCBio import GFF
 from pathlib import Path
+from collections import defaultdict
+from skbio.io import read
 from sql_initialize_db import initialize_db
 from sql_connection import TRN
 
-def read_gff_file(gff_file):
+# Define constants for table names
+TABLE_NAMES = ["RefSeq", "SO", "UniParc", "UniRef", "KEGG", "PFAM"]
+
+# Define SQL templates for inserting data into different tables
+INSERT_SQL_TEMPLATE = """INSERT INTO {} (bakta_accession, {})
+                        VALUES (?, ?)"""
+
+def read_gff_file(gff_file: str) -> list:
+    """
+    Read GFF file using skbio.
+
+    Args:
+        gff_file (str): Path to the GFF file.
+
+    Returns:
+        list: List of records from the GFF file.
+    """
     if not Path(gff_file).exists():
         raise FileNotFoundError(f"Error: GFF file not found at {gff_file}")
-    records = []
-    with open(gff_file, "r") as in_handle:
-        records = list(GFF.parse(in_handle))
-    return records
+    return read(gff_file, format="gff3")
 
-def process_qualifiers(qualifiers, qualifier_keys):
-    processed_qualifiers = {}
-    for key in qualifier_keys:
-        value = qualifiers.get(key, None)
-        if isinstance(value, list):
-            processed_value = ", ".join(value)
-        else:
-            processed_value = value
-        processed_qualifiers[key] = processed_value
-    return processed_qualifiers
+def process_dbxref(dbxref_entry: str) -> defaultdict:
+    """
+    Process dbxref entry and return a dictionary.
 
-def process_dbxref(dbxref_entry):
-    entry_dict = {}
+    Args:
+        dbxref_entry (str): Dbxref entry string.
+
+    Returns:
+        defaultdict: Processed dbxref entry as a dictionary.
+    """
+    entry_dict = defaultdict(list)
     if dbxref_entry:
-        entries = dbxref_entry.split(',')
-        for entry in entries:
-            entry_type, entry_value = entry.split(':')
-            entry_value = entry_value.strip()
-            entry_type = entry_type.strip()
-            if entry_type not in entry_dict:
-                entry_dict[entry_type] = [entry_value]
-            else:
-                entry_dict[entry_type].append(entry_value)
-    else:
-        entry_dict = {}
-    dbxref_edit = {}
-    for key, value in entry_dict.items():
-        if isinstance(value, list):
-            value_str = ', '.join(value)
-            dbxref_edit[key] = value_str
-        else:
-            dbxref_edit[key] = value
-    return dbxref_edit
+        entries = [entry.split(':') for entry in dbxref_entry.split(',')]
+        for entry_type, entry_value in entries:
+            entry_dict[entry_type.strip()].append(entry_value.strip())
+    return entry_dict
 
-def insert_bakta(entity_id, contig_id, position, gene_id, source, type, start, end, strand, phase, 
-                name, product, refseq, so, uniparc, uniref, kegg, pfam, run_accession):
-    sql = "INSERT INTO bakta (entity_id, contig_id, position, gene_id, source, type, start, end, strand, phase, name, product, RefSeq, SO, UniParc, Uniref, KEGG, PFAM, run_accession) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
-    args = [entity_id, contig_id, position, gene_id, source, type, start, end, strand, phase, 
-                name, product, refseq, so, uniparc, uniref, kegg, pfam, run_accession]
-    with TRN: 
+def insert_into_table(table_name: str, bakta_accession: int, values: list):
+    """
+    Insert values into a specified table.
+
+    Args:
+        table_name (str): Name of the table.
+        bakta_accession (int): Bakta accession identifier.
+        values (list): List of values to insert into the table.
+    """
+    # I made a different table for each TABLE_NAME
+    sql = INSERT_SQL_TEMPLATE.format(table_name, table_name.lower())
+
+    with TRN:
+        for value in values:
+            args = [bakta_accession, value]
+            TRN.add(sql, args)
+            TRN.execute()
+
+def insert_bakta(entity_id: str, run_accession: int, contig_id: int, position: int, interval) -> int:
+    """
+    Insert data into bakta table and return bakta_accession.
+
+    Args:
+        entity_id (str): Entity identifier.
+        run_accession (int): Run accession identifier.
+        contig_id (int): Contig identifier.
+        position (int): Position of the interval.
+        interval: Interval object.
+
+    Returns:
+        int: Bakta accession identifier.
+    """
+    sql = """INSERT INTO bakta (entity_id, contig_id, position, gene_id, source, type, start, end, 
+                               strand, phase, name, product, run_accession) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            returning bakta_accession"""
+    args = [entity_id, contig_id, position, interval.metadata.get("ID"),
+            interval.metadata.get("source"), interval.metadata.get("type"), 
+            interval.bounds[0][0], interval.bounds[0][1], interval.metadata.get("strand"),
+            interval.metadata.get("phase"), interval.metadata.get("Name"),
+            interval.metadata.get("product"), run_accession]
+
+    with TRN:
         TRN.add(sql, args)
-        TRN.execute()
+        return TRN.execute_fetchflatten()[0]
 
-def parse_bakta(entity_id, temp_dir, run_accession):
-    qualifier_keys = ["ID", "source", "Name", "locus_tag", "product", "Dbxref"]
+def main_bakta(entity_id: str, temp_dir: str, run_accession: int) -> int:
+    """
+    Process bakta data and insert into the database.
+
+    Args:
+        entity_id (str): Entity identifier.
+        temp_dir (str): Path to the temporary directory.
+        run_accession (int): Run accession identifier.
+
+    Returns:
+        int: Return value (usually 0).
+    """
+
+    # Wasn't sure if this function needed to be divided further into smaller chunks
     gff_file = f"{temp_dir}/{entity_id}.gff3"
     contig_id = 0
 
     for rec in read_gff_file(gff_file):
-        for feature in rec.features:
-            qualifiers = feature.qualifiers
-            processed_qualifiers = process_qualifiers(qualifiers, qualifier_keys)
-            dbxref_entry = processed_qualifiers.get("Dbxref", "")
-            dbxref_edit = process_dbxref(dbxref_entry)
+        contig_id, interval_metadata = rec[0], rec[1]
+        # I couldn't figure out a different way to convert the interval_metadata object to a list 
+        interval_metadata = list(interval_metadata.query(metadata={'score':'.'}))
 
-            # Increment position for each gene on a contig
-            if contig_id:
-                if contig_id == rec.id:
-                    position += 1
-                else:
-                    position = 0
-            else:
-                contig_id = rec.id
-                position = 1
+        for position, interval in enumerate(interval_metadata[1:], start=1):
+            bakta_accession = insert_bakta(entity_id, run_accession, contig_id, position, interval)
 
-            insert_bakta(entity_id, rec.id, position, 
-                         processed_qualifiers.get("ID", None),
-                         processed_qualifiers.get("source", None), feature.type,
-                         int(feature.location.start), int(feature.location.end),
-                         feature.location.strand, processed_qualifiers.get("phase", None),
-                         processed_qualifiers.get("Name", None),
-                         processed_qualifiers.get("product", None),
-                         dbxref_edit.get("RefSeq", None), dbxref_edit.get("SO", None), dbxref_edit.get("UniParc", None),
-                         dbxref_edit.get("UniRef", None), dbxref_edit.get("KEGG", None), dbxref_edit.get("PFAM", None), run_accession)
+            if 'db_xref' in interval.metadata:
+                db_xref_dict = process_dbxref(interval.metadata['db_xref'])
+
+                for table_name in TABLE_NAMES:
+                    values = db_xref_dict.get(table_name)
+                    if values:
+                        insert_into_table(table_name, bakta_accession, values)
+
     return 0
-
-def delete_bakta_output_files(bakta_output, temp_dir):
-    bakta_output_path = os.path.join(temp_dir, bakta_output)
-    if os.path.exists(bakta_output_path):
-        try:
-            if os.path.isfile(bakta_output_path):
-                os.remove(bakta_output_path)
-            elif os.path.isdir(bakta_output_path):
-                shutil.rmtree(bakta_output_path)
-            else:
-                print(f"Warning: Unrecognized file/directory type at {bakta_output_path}")
-        except Exception as e:
-            print(f"Error deleting Bakta output: {e}")
-            return 1  # Return a non-zero code for failure
-        return 0  # Return 0 for success
-    else:
-        print(f"Error: Bakta output path does not exist: {bakta_output_path}")
-        return 1  # Return a non-zero code for failure
 
 # Example usage:
 if __name__ == "__main__":
     initialize_db()
-    temp_dir = "/Users/reneeoles/Desktop/bakta_plan/redgenes/redgenes/tests/data/"
+    temp_dir = "/Users/reneeoles/Desktop/bakta_plan/redgenes/redgenes/tests/data"    
     entity_id = "GCA_001699855.1"
     bakta_run_accession = 1
-    parse_bakta(str(entity_id), temp_dir, bakta_run_accession)
+    main_bakta(str(entity_id), temp_dir, bakta_run_accession)
     entity_id = "GCA_008569345.1"
     bakta_run_accession = 2
-    parse_bakta(str(entity_id), temp_dir, bakta_run_accession)
-
+    main_bakta(str(entity_id), temp_dir, bakta_run_accession)
